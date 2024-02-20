96966d15af82ce7d3ea51e2164a0431ae1ad614d
Prabin Upreti <upretirajprabin@gmail.com>
2024-02-20 19:55:08 +0545

    fix speedinsight vercel

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
-import {} from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
